cd757c14a6b5a425cebe8292cc171bb20c71e4f0
William Bui <williammbui@gmail.com>
2019-05-17 01:04:41 -0400

    filters and scene

---
+++ Assets/BlocksAndFilters/Scripts/Scroll_UV.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll_UV : MonoBehaviour {
+    float scrollSpeed_X = 0.05f;
+    float scrollSpeed_Y = 0.05f;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    
+    void Update() {
+        var offsetX = Time.time * scrollSpeed_X;
+        var offsetY = Time.time * scrollSpeed_Y;
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(offsetX, offsetY);
+    }
+}
